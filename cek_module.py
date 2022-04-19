b2dd117e09c981b025bb9173f395e44533914948
SindyAul <103235378+4se1L@users.noreply.github.com>
2022-04-19 19:18:23 +0700

    Delete cek_module.py

--- cek_module.py
+++
@@ -1,8 +0,0 @@
-import time,sys
-def run(arg,waktu):
-	for h in arg+"\n":
-		sys.stdout.write(h)
-		sys.stdout.flush()
-		time.sleep(waktu)
-if __name__ == '__main__':
-	run("\033[33m[#] Sabar tot lagi ngecek module",0.1)ýÿ
